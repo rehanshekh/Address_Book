e7393f6252cf457f88878d9a6f1a3acc779450d4
Rehan Shekh <rehanrshekh@gmail.com>
2022-03-05 01:21:29 +0530

    Added ability to edit a contact by there First Name

--- src/com/oops/addressBookMain.java
+++ src/com/oops/addressBookMain.java
@@ -1,12 +1,35 @@
 package com.oops;
 
+import java.util.Scanner;
+
 public class addressBookMain {
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Address Book Program");
-        addressBook contactinfo = new addressBook();
-        contactinfo.setUpInfo();
-        contactinfo.displayListItems();
+        addressBookMain iterateObj = new addressBookMain();
+        iterateObj.iterateOps();
+    }
 
+    private void iterateOps() {
+        addressBook contactInfo = new addressBook();
+        contactInfo.setUpInfo();
+        contactInfo.displayListItems();
+        int i;
+        int j = 0;
+        for (i = 1; i > j; i++) {
+            System.out.println("Press 1 to continue adding contacts/Press 2 to edit a contact/Press 0 to exit the program");
+            Scanner in = new Scanner(System.in);
+            int operation = in.nextInt();
+            if (operation == 1) {
+                contactInfo.setUpInfo();
+                contactInfo.displayListItems();
+            } else if (operation == 2) {
+                contactInfo.editContact();
+            } else if (operation == 0) {
+                j = i + 2;
+            }
+        }
     }
 }
+
+
