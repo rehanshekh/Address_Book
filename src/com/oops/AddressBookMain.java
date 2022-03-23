52e2173b1d59147bcaa2adeb1a827aca77b1641c
Rehan Shekh <rehanrshekh@gmail.com>
2022-03-24 01:37:01 +0530

    Added Hashmap, to create multiple Address Books

--- src/com/oops/AddressBookMain.java
+++ src/com/oops/AddressBookMain.java
@@ -13,19 +13,20 @@ public class AddressBookMain {
         int i;
         int j = 0;
         for (i = 1; i > j; i++) {
-            System.out.println("Press 1 to continue adding contacts/Press 2 to edit a contact/Press 3 to delete a contact/Press 0 to exit the program");
+            System.out.println("Press 1 to continue creating Address Book/Press 2 to edit a contact in an Address Book/Press 3 to delete a contact from an Address Book/Press 4 to Add a New Contact to an Address Book/Press 0 to exit the program");
             int operation = in.nextInt();
             if (operation == 1) {
                 contactInfo.setUpInfo();
                 contactInfo.displayListItems();
-                System.out.println("Total Contacts for Address Book 1:"+contactInfo.getcompanyName("DMart"));
             } else if (operation == 2) {
                 contactInfo.editContact();
-                contactInfo.displayListItems();
+
             } else if (operation == 0) {
                 j = i + 2;
             } else if (operation == 3) {
                 contactInfo.deleteContact();
+            } else if (operation == 4) {
+                contactInfo.add();
             }
         }
     }
